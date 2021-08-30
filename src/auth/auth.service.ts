28bdc14b086a8e8d9fc69e9bbe82b1cdb69e74f0
Sergiy <serguz@ukr.net>
2021-08-30 18:26:16 +0300

    login and registration endpoints implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpService, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcryptjs';
+import { User } from 'src/users/users.model';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private userService: UsersService,
+                private jwtService: JwtService) {}
+
+    async login(userDto: CreateUserDto) {
+        const user = await this.validateUser(userDto);
+        return this.generateToken(user);
+    }
+
+    async registration(userDto: CreateUserDto) {
+        const candidate = await this.userService.getUserByEmail(userDto.email);
+        if (candidate) {
+            throw new HttpException('Этот имейл уже занят! Выберите другой имей', HttpStatus.BAD_REQUEST);
+        }
+        //хэшируем пароль с использованием "соли ": 5
+        const hashPassword = await bcrypt.hash(userDto.password, 5);
+        const user = await this.userService.createUser({...userDto, password: hashPassword});
+        return this.generateToken(user)
+    }
+
+    private async generateToken(user: User) {
+        const payload = {email: user.email, id: user.id, roles: user.roles};
+        return {
+            token: this.jwtService.sign(payload)
+        }
+    }
+
+    private async validateUser(userDto: CreateUserDto) {
+        const user = await this.userService.getUserByEmail(userDto.email);
+        const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+        if (user && passwordEquals) {
+            return user;
+        }
+        throw new UnauthorizedException({message: 'Неверный имейл или пароль'});
+    }
+}
